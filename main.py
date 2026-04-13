985ab998a5409f2e537dbd4f610dadb78328d020
Mostafa <mostafa_d1990@yahoo.com>
2026-04-13 19:40:42 +0200

    c

--- main.py
+++ main.py
@@ -159,6 +159,7 @@ def main():
             print(f"Finished writing results to {filename}")
 
         except Exception as e:
+            print('AAAAA')
             print(f"API request failed: {e}")
             if response is not None:
                 print("Status code:", response.status_code)
